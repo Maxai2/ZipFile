bee8b4cb1a1b0d63dc489f233d0dd6ae81dcdfbb
Хамелеон <maxali946@gmail.com>
2018-06-27 18:12:58 +0400

    40%

--- ZipFile/MainWindow.xaml.cs
+++ ZipFile/MainWindow.xaml.cs
@@ -1,31 +1,161 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZipFile
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool isZip = true;
+        public bool IsZip
+        {
+            get => isZip;
+            set
+            {
+                isZip = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string filePath = "";
+        public string FilePath
+        {
+            get => filePath;
+            set
+            {
+                filePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        OpenFileDialog OpenFile;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+
+            OpenFile = new OpenFileDialog();
+            OpenFile.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            OpenFile.RestoreDirectory = true;
         }
+
+        //--------------------------------------------------------------------
+
+        private ICommand fileSelect;
+        public ICommand FileSelect
+        {
+            get
+            {
+                if (fileSelect is null)
+                {
+                    fileSelect = new RelayCommand(
+                        (param) =>
+                        {
+                            if (OpenFile.ShowDialog() == true)
+                            {
+                                FilePath = OpenFile.FileName;
+                            }
+                        });
+                }
+
+                return fileSelect;
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        string fileText;
+
+        private ICommand startCom;
+        public ICommand StartCom
+        {
+            get
+            {
+                if (startCom is null)
+                {
+                    startCom = new RelayCommand(
+                        (param) =>
+                        {
+                            using (FileStream fstream = File.OpenRead(FilePath))
+                            {
+                                byte[] array = new byte[fstream.Length];
+
+                                fstream.Read(array, 0, array.Length);
+
+                                fileText = Encoding.Default.GetString(array);
+                            }
+
+                            var fileLength = fileText.Length;
+
+                            if (fileLength < 1000)
+                            {
+
+                            }
+                            else
+                            if (1000 <= fileLength && fileLength < 2000)
+                            {
+
+                            }
+                            else
+                            if (2000 <= fileLength && fileLength < 4000)
+                            {
+
+                            }
+                            else
+                            if (4000 <= fileLength && fileLength < 8000)
+                            {
+
+                            }
+                            else
+                            if (8000 <= fileLength && fileLength < 16000)
+                            {
+
+                            }
+                            else
+                            if (16000 <= fileLength)
+                            {
+
+                            }
+
+                        });
+                }
+
+                return startCom;
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        private ICommand cancelCom;
+        public ICommand CancelCom
+        {
+            get
+            {
+                if (cancelCom is null)
+                {
+                    cancelCom = new RelayCommand(
+                        (param) =>
+                        {
+
+                        });
+                }
+
+                return cancelCom;
+            }
+        }
+
+        //--------------------------------------------------------------------
 
         public static byte[] ZipStr(String str)
         {
@@ -43,6 +173,27 @@ namespace ZipFile
             }
         }
 
+        //public static void Compress(FileInfo fi)
+        //{
+        //    using (FileStream inFile = fi.OpenRead())
+        //    {
+        //        if ((File.GetAttributes(fi.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & fi.Extension != ".cmp")
+        //        {
+        //            using (FileStream outFile = File.Create(fi.FullName + ".cmp"))
+        //            {
+        //                using (DeflateStream Compress = new DeflateStream(outFile, CompressionMode.Compress))
+        //                {
+        //                    inFile.CopyTo(Compress);
+
+        //                    //Console.WriteLine("Compressed {0} from {1} to {2} bytes.", fi.Name, fi.Length.ToString(), outFile.Length.ToString());
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        //--------------------------------------------------------------------
+
         public static string UnZipStr(byte[] input)
         {
             using (MemoryStream inputStream = new MemoryStream(input))
@@ -56,5 +207,35 @@ namespace ZipFile
                 }
             }
         }
+
+        //public static void Decompress(FileInfo fi)
+        //{
+        //    using (FileStream inFile = fi.OpenRead())
+        //    {
+        //        string curFile = fi.FullName;
+        //        string origName = curFile.Remove(curFile.Length - fi.Extension.Length);
+
+        //        using (FileStream outFile = File.Create(origName))
+        //        {
+        //            using (DeflateStream Decompress = new DeflateStream(inFile, CompressionMode.Decompress))
+        //            {
+        //                Decompress.CopyTo(outFile);
+
+        //                //Console.WriteLine("Decompressed: {0}", fi.Name);
+        //            }
+        //        }
+        //    }
+        //}
+
+        //--------------------------------------------------------------------
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        //--------------------------------------------------------------------
     }
 }
--- ZipFile/RelayCommand.cs
+++ ZipFile/RelayCommand.cs
@@ -9,18 +9,29 @@ namespace ZipFile
 {
     public class RelayCommand : ICommand
     {
-        
+        private Action<object> execute;
+        private Predicate<object> canExecute;
 
-        public event EventHandler CanExecuteChanged;
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return this.canExecute?.Invoke(parameter) ?? true;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            this.execute.Invoke(parameter);
         }
     }
 }
